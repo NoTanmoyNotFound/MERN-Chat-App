e776cfc4c37169793afcb4d12bfd823885493517
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2025-03-28 11:43:24 +0530

    adding components

---
+++ frontend/src/components/ChatContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatContainer = () => {
+  return (
+    <div>
+      chat conatiner
+    </div>
+  )
+}
+
+export default ChatContainer
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useChatStore } from '../store/useChatStore'
-import { Sidebar } from 'lucide-react'
+import Sidebar from '../components/Sidebar'
+import ChatContainer from '../components/ChatContainer'
+import NoChatSelected from '../components/NoChatSelected'
+
 
 const HomePage = () => {
   const {selectedUser} = useChatStore()
@@ -14,7 +17,7 @@ const HomePage = () => {
           <div className='flex h-full rounded-lg overflow-hidden'>
             <Sidebar />
 
-            {/* {!selectedUser ? <NoChatSelected /> : <ChatContainer />} */}
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
 
           </div>
 
